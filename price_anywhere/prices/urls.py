e8fa7134762ba74d9fdcdf7354ef4fd826c47305
tobyatgithub <toby.fangyuan@gmail.com>
2023-12-17 14:13:26 -0600

    Add function to adding records.

--- price_anywhere/prices/urls.py
+++ price_anywhere/prices/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PurchaseRecordListView
+from .views import PurchaseRecordListView, PurchaseRecordCreateView
 
 urlpatterns = [
     path('purchase_records/', PurchaseRecordListView.as_view(), name='purchase_record_list'),
+    path('purchase_records/add/', PurchaseRecordCreateView.as_view(), name='purchase_record_add'),
     # Add more URL patterns for other views (create, update, delete) as needed
 ]
--- price_anywhere/prices/views.py
+++ price_anywhere/prices/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import PurchaseRecord
+from .forms import PurchaseRecordForm
 
 # Create your views here.
+class PurchaseRecordCreateView(CreateView):
+    model = PurchaseRecord
+    form_class = PurchaseRecordForm
+    template_name = 'prices/purchase_record_form.html'
+    success_url = reverse_lazy('purchase_record_list')
+
 class PurchaseRecordListView(ListView):
     model = PurchaseRecord
     template_name = 'prices/purchase_record_list.html'
