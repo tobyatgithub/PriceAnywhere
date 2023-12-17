31f543bfd80ea07996014146ceee870e0ed47089
tobyatgithub <toby.fangyuan@gmail.com>
2023-12-17 14:37:19 -0600

    Add delete functionality

--- price_anywhere/prices/urls.py
+++ price_anywhere/prices/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PurchaseRecordListView, PurchaseRecordCreateView, PurchaseRecordUpdateView
+from .views import PurchaseRecordListView, PurchaseRecordCreateView, PurchaseRecordUpdateView, PurchaseRecordDeleteView
 
 urlpatterns = [
     path('purchase_records/', PurchaseRecordListView.as_view(), name='purchase_record_list'),
     path('purchase_records/add/', PurchaseRecordCreateView.as_view(), name='purchase_record_add'),
     path('purchase_records/<int:pk>/edit/', PurchaseRecordUpdateView.as_view(), name='purchase_record_edit'),
+    path('purchase_records/<int:pk>/delete/', PurchaseRecordDeleteView.as_view(), name='purchase_record_delete'),
     # Add more URL patterns for other views (create, update, delete) as needed
 ]
--- price_anywhere/prices/views.py
+++ price_anywhere/prices/views.py
@@ -2,7 +2,7 @@ from django.forms.models import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import PurchaseRecord
 from .forms import PurchaseRecordForm
@@ -28,3 +28,8 @@ class PurchaseRecordUpdateView(UpdateView):
         # Prepopulate the form with existing data for editing
         form.instance = self.get_object()
         return super().form_valid(form)
+    
+class PurchaseRecordDeleteView(DeleteView):
+    model = PurchaseRecord
+    template_name = 'prices/purchase_record_confirm_delete.html'
+    success_url = reverse_lazy('purchase_record_list')
