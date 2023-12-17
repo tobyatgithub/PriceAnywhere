6772be1cc31d25e764e5bef55a882073c988bdd3
tobyatgithub <toby.fangyuan@gmail.com>
2023-12-17 14:20:21 -0600

    Add edit functionality.
    Reused the purchase_record_form.html, add logic to distinguish between
    add and edit. Also updated views.py to pre-populate data if editing.

--- price_anywhere/prices/urls.py
+++ price_anywhere/prices/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PurchaseRecordListView, PurchaseRecordCreateView
+from .views import PurchaseRecordListView, PurchaseRecordCreateView, PurchaseRecordUpdateView
 
 urlpatterns = [
     path('purchase_records/', PurchaseRecordListView.as_view(), name='purchase_record_list'),
     path('purchase_records/add/', PurchaseRecordCreateView.as_view(), name='purchase_record_add'),
+    path('purchase_records/<int:pk>/edit/', PurchaseRecordUpdateView.as_view(), name='purchase_record_edit'),
     # Add more URL patterns for other views (create, update, delete) as needed
 ]
--- price_anywhere/prices/views.py
+++ price_anywhere/prices/views.py
@@ -1,18 +1,30 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import PurchaseRecord
 from .forms import PurchaseRecordForm
 
-# Create your views here.
+class PurchaseRecordListView(ListView):
+    model = PurchaseRecord
+    template_name = 'prices/purchase_record_list.html'
+    context_object_name = 'purchase_records'
+
 class PurchaseRecordCreateView(CreateView):
     model = PurchaseRecord
     form_class = PurchaseRecordForm
     template_name = 'prices/purchase_record_form.html'
     success_url = reverse_lazy('purchase_record_list')
 
-class PurchaseRecordListView(ListView):
+class PurchaseRecordUpdateView(UpdateView):
     model = PurchaseRecord
-    template_name = 'prices/purchase_record_list.html'
-    context_object_name = 'purchase_records'
+    form_class = PurchaseRecordForm
+    template_name = 'prices/purchase_record_form.html'
+    success_url = reverse_lazy('purchase_record_list')
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        # Prepopulate the form with existing data for editing
+        form.instance = self.get_object()
+        return super().form_valid(form)
