e7cfe9bc7cc307730706cb14731f71163c5d76ff
tobyatgithub <toby.fangyuan@gmail.com>
2023-12-18 10:51:01 -0600

    Modify model details. Keep two separate models.
    And to add more.

--- price_anywhere/prices/models.py
+++ price_anywhere/prices/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from products.models import Product
+## TODO: 2023-12-17:
+## I think its good idea from gpt to add the following models:
+## User, Category, Store.
+
 
 # Purchase record, each row is one product bought.
 class PurchaseRecord(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    productId = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True, editable=False)
+    product_name = models.CharField(max_length=255, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     price_unit = models.CharField(max_length=255, null=True, blank=True)
     weight = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
@@ -16,3 +21,5 @@ class PurchaseRecord(models.Model):
     has_discount = models.BooleanField(default=False)
     notes = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.date} - {self.product_name}"  # Adjust the representation as needed
