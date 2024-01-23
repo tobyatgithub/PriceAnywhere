813a50ef9e0f123b5762b4f8ef4f6097a58e9972
tobyatgithub <toby.fangyuan@gmail.com>
2024-01-24 01:46:23 +0800

    Fix wordings on home page.

--- price_anywhere/prices/urls.py
+++ price_anywhere/prices/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import PurchaseRecordListView, PurchaseRecordCreateView, PurchaseRecordUpdateView, PurchaseRecordDeleteView
 
 urlpatterns = [
+    path('', PurchaseRecordListView.as_view(), name='purchase_record_list'),
     path('purchase_records/', PurchaseRecordListView.as_view(), name='purchase_record_list'),
     path('purchase_records/add/', PurchaseRecordCreateView.as_view(), name='purchase_record_add'),
     path('purchase_records/<int:pk>/edit/', PurchaseRecordUpdateView.as_view(), name='purchase_record_edit'),
